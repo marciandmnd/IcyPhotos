719fd06b04731d0f77a9217c099f5080b1760f70
Marcian Diamond <marciandmnd@yahoo.com>
2016-12-01 16:51:31 +0100

    updated model to use mongodb mongolab

--- models/Photo.js
+++ models/Photo.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/photo_app');
+mongoose.connect('mongodb://heroku_v388z7qx:nim5f96lkkcbnp28t2nid33h0l@ds115798.mlab.com:15798/heroku_v388z7qx');
 
 var schema = new mongoose.Schema({
   name: String,
