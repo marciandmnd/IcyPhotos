31810938310935d37190be572eb58e49f0e76343
Marcian Diamond <marciandmnd@yahoo.com>
2016-12-01 16:01:36 +0100

    added node js sprite and bass css module

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/basscss', express.static(__dirname + '/node_modules/basscss/css/'));
 
 app.get('/', photos.list);
 app.get('/upload', photos.form);
